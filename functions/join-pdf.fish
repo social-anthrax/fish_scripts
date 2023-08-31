f4b577f31d675533ffd69714a77288228e5e7d63
Artemis Livingstone <ar.d.livingstone@gmail.com>
2023-08-30 22:09:13 -0400

    remove broken description

--- functions/join-pdf.fish
+++ functions/join-pdf.fish
@@ -1,4 +1,4 @@
 function join-pdf
-    argparse --name="join_pdf" --description="MACOS Specific. Uses ghostscript to join multiple pdf files" "o/output=" -- $argv
+    argparse --name="join_pdf" "o/output=" -- $argv
     gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="$_flag_output" $argv
 end
